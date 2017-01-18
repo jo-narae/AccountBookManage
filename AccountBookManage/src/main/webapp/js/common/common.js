e0023cb460cf38735c85aacf280105d3f361923a
jo-narae <nare6004@gmail.com>
2017-01-19 01:00:34 +0900

    공통 css, fonts, img, js 폴더 및 파일 생성

---
+++ AccountBookManage/src/main/webapp/js/common/common.js
@@ -0,0 +1,26 @@
+/**
+ * 공통
+ */
+
+function roomList() {
+	location.href = "../room/roomList.do?pageNum=1";
+}
+
+//내정보
+function myInfo() {
+	location.href = "../info/myInfo.do";
+}
+
+//로그아웃
+function sessionLogin() {
+	location.href = "../user/userLogin.do";
+}
+
+//로그아웃
+function sessionLogout() {
+	if (confirm("로그아웃 하시겠습니까?") == true){
+		location.href = "../user/sessionLogout.do";
+	}else{
+	    return;
+	}
+}
