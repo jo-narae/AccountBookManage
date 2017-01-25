9fbcb4ff61e7a606c0dc13531eba2d5434df56a8
Ham-MinJeong <gaw0901@nate.com>
2017-01-25 23:22:04 +0900

    테스트1

--- AccountBookManage/src/main/java/com/msw/abm/controller/MinjeongController.java
+++ AccountBookManage/src/main/java/com/msw/abm/controller/MinjeongController.java
@@ -15,7 +15,7 @@ public class MinjeongController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * 내정보 화면
+	 * 내정보 화면1
 	 * @return
 	 */
 	@RequestMapping("deposit/depositList.do")
