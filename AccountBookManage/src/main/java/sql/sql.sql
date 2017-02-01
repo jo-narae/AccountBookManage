5e412e8dc50e77b11e73ed86645db9d40c9e7172
jo-narae <nare6004@gmail.com>
2017-02-01 22:21:09 +0900

    Table 생성 쿼리

--- AccountBookManage/src/main/java/sql/sql.sql
+++ AccountBookManage/src/main/java/sql/sql.sql
@@ -1,65 +1,36 @@
-﻿
-
-//여기 하단부턴 죄다 임시
--- test query --
-CREATE TABLE room_list
+﻿-- 영수증 테이블
+CREATE TABLE receipt
 (
-	seq int NOT NULL AUTO_INCREMENT primary key,
-	room_name varchar(255) NOT NULL,
-	room_master varchar(20) NOT NULL,
-	date datetime not null
-)
+  id int NOT NULL AUTO_INCREMENT primary key,
+  src varchar(255) NOT NULL
+);
 
-CREATE TABLE user_chat
-(
-room_seq int not null,
-chat_seq int NOT NULL AUTO_INCREMENT primary key,
-user_name varchar(20) NOT NULL,
-content varchar(255) NOT NULL,
-date datetime not null,
-FOREIGN KEY (room_seq) REFERENCES room_list (seq) on delete cascade
-)
 
-/**
-*  test
-*/
-CREATE TABLE user_chat
+-- 거래 테이블
+CREATE TABLE deal
 (
-room_seq int,
-chat_seq int NOT NULL AUTO_INCREMENT primary key,
-user_name varchar(20) NOT NULL,
-content varchar(255) NOT NULL,
-date datetime not null
-)
+	id int NOT NULL AUTO_INCREMENT primary key,
+  user_name varchar(10) NOT NULL,
+	content varchar(255) NOT NULL,
+  price int NOT NULL,
+	balance int NOT NULL,
+	deal_date datetime NOT NULL,
+  deal_type varchar(10) NOT NULL,
+  deposit_account int,
+  receipt_id int,
+  FOREIGN KEY (receipt_id) REFERENCES receipt(id) ON DELETE CASCADE
+);
 
-/**
- * 최종
- */
-CREATE TABLE room_list
+-- 신청 테이블
+CREATE TABLE apply
 (
-	seq int NOT NULL AUTO_INCREMENT primary key,
-	room_title varchar(255) NOT NULL,
-	master_id varchar(20) NOT NULL,
-	master_name varchar(20) NOT NULL,
-	date datetime not null
-)
-
-CREATE TABLE user_chat
-(
-room_seq int not null,
-chat_seq int NOT NULL AUTO_INCREMENT primary key,
-user_name varchar(20) NOT NULL,
-user_id varchar(20) NOT NULL,
-content varchar(255) NOT NULL,
-date datetime not null,
-FOREIGN KEY (room_seq) REFERENCES room_list (seq) on delete cascade
-)
-
-CREATE TABLE user_info
-(
-user_id varchar(20) NOT NULL primary key,
-user_name varchar(20) NOT NULL,
-user_mail varchar(50) NOT NULL,
-user_pwd varchar(50) NOT NULL,
-date datetime not null
-)
+	id int NOT NULL AUTO_INCREMENT primary key,
+  user_name varchar(10) NOT NULL,
+	content varchar(255) NOT NULL,
+  price int NOT NULL,
+	apply_date datetime NOT NULL,
+  apply_type varchar(10) NOT NULL,
+  deposit_account int,
+  receipt_id int,
+  FOREIGN KEY (receipt_id) REFERENCES receipt(id) ON DELETE CASCADE
+);
