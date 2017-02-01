afacae30be8832c365099a31081b3a4c602e046d
jo-narae <nare6004@gmail.com>
2017-02-02 00:09:30 +0900

    거래 테이블 가라 데이터 insert문 추가

--- AccountBookManage/src/main/java/sql/sql.sql
+++ AccountBookManage/src/main/java/sql/sql.sql
@@ -21,6 +21,17 @@ CREATE TABLE deal
   FOREIGN KEY (receipt_id) REFERENCES receipt(id) ON DELETE CASCADE
 );
 
+-- 거래 테이블 INSERT
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("이주형", "인수인계", "130000", "130000", NOW(), "DEPOSIT");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("함민정", "공동 USB 구입", "10000", "120000", NOW(), "WITHDRAW");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("조나래", "커피 구입", "30000", "90000", NOW(), "WITHDRAW");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("이주형", "회비납부", "30000", "120000", NOW(), "DEPOSIT");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("김민석", "회비납부", "30000", "150000", NOW(), "DEPOSIT");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("이현석", "회비납부", "30000", "180000", NOW(), "DEPOSIT");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("조나래", "회비납부", "30000", "210000", NOW(), "DEPOSIT");
+INSERT INTO deal (user_name, content, price, balance, deal_date, deal_type ) values ("함민정", "졸업생 선물", "50000", "160000", NOW(), "WITHDRAW");
+
+
 -- 신청 테이블
 CREATE TABLE apply
 (
